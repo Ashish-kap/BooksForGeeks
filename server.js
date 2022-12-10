c37df0abb1ae7e39a2a63fc1dffc0e1fa038b57a
Ashish kap <ashishkap157@gmail.com>
2022-12-11 01:09:18 +0530

    updated server.js

--- server.js
+++ server.js
@@ -8,7 +8,8 @@ mongoose.connect('mongodb+srv://srigbok:test1234@cluster0.oj5qw.mongodb.net/Book
     console.log('database connection successful')
 })
 
-app.listen(3000,()=>{
+const port = process.env.PORT || 3000
+app.listen(port,()=>{
     console.log('server started')
 })
 
